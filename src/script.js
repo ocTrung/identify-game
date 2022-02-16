97b3a25d7b2239dc027d141da59b1c971e022b52
ocTrung <t.nguyenvo@eagles.oc.edu>
2022-02-15 18:37:31 -0600

    initial list of questions for testing

---
+++ src/script.js
@@ -0,0 +1,18 @@
+const questions = {
+  oot: {
+    img: 'https://imgur.com/LrBgi5B',
+    correct: 'Ocarina of Time'
+  },
+  ff: {
+    img: 'https://imgur.com/JWcQbmY',
+    correct: 'Final Fantasy'
+  },
+  fallout: {
+    img: 'https://imgur.com/97AMcyX',
+    correct: 'Fall Out'
+  },
+  pokemon: {
+    img: 'https://imgur.com/4UqEVSK',
+    correct: 'Pokemon'
+  },
+}
