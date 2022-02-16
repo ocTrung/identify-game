1c45c2b205eb5e908f36fa8e68da048b92d30967
ocTrung <t.nguyenvo@eagles.oc.edu>
2022-02-15 22:59:33 -0600

    quick ver 1 of game complete

--- src/script.js
+++ src/script.js
@@ -24,26 +24,54 @@ const games = questions.map(({game}) => {
 const questionDisplay = document.getElementById('question-display')
 const answersDisplay = document.getElementById('answers-display')
 const imgDisplay = document.getElementById('img-display')
+const startButton = document.getElementById('start-btn')
+const quizContainer = document.querySelector('.quiz-container')
+const menu = document.querySelector('.menu')
 let index = 0
 let correctCount = 0
+startButton.onclick = init
 
 function init() {
+  menu.classList.add('hidden')
+  quizContainer.classList.remove('hidden')
+
   index = 0
   correctCount = 0
   displayNewQuestion()
-  index++
+}
+
+const onAnswerButtonClick = (correct, choice) => {
+  if (choice === correct) {
+    correctCount++
+  }
+  if (isGameOver()) {
+    displayResults()
+  } else {
+    index++
+    displayNewQuestion()
+  }
+  
 }
 
 function displayNewQuestion() {
+  // apply image of current question to html
   imgDisplay.src = questions[index].img
+  // get a random set of 4 choices from all possible answers. Includes correct answer.
+  // this is a list of indexes
   let answerIndexList = getRandomAnswers(index)
+  // reference to correct answer using list of questions + index
+  const correct = questions[index].game
 
+  // remove previous choices
+  answersDisplay.innerHTML = ''
+
+  // generate buttons for choices
   answerIndexList.forEach(ansIdx => {
-    const button = document.createElement('button')
-    button.textContent = games[ansIdx]
-    button.addEventListener('click', () => console.log(`clicked ${games[ansIdx]}`)
-    )
-    answersDisplay.append(button)
+    const answerButton = document.createElement('button')
+    const choice = games[ansIdx]
+    answerButton.textContent = choice
+    answerButton.addEventListener('click', () => onAnswerButtonClick(correct, choice))
+    answersDisplay.append(answerButton)
   })
 }
 
@@ -59,16 +87,25 @@ function getRandomAnswers(correctIndex) {
       randomAnswers.push(randIndex)
     randIndex = getRandIndex()
   }
-  return randomAnswers
+  return sortAnswersRandom(randomAnswers)
 }
 
 function sortAnswersRandom(list) {
   return list.sort((a,b) => Math.floor(Math.random() * 2) - 1)
 }
 
-init()
+function isGameOver() {
+  console.log('index', index)
+  console.log('length', questions.length)
+  return index === questions.length -1
+}
 
-let list = getRandomAnswers(0)
-console.log('unsorted', list)
-let sorted = sortAnswersRandom(list)
-console.log('sorted', sorted)
+function displayResults() {
+  const results = document.getElementById('results')
+  const resultsContainer = document.querySelector('.results-container')
+
+  quizContainer.classList.add('hidden')
+  resultsContainer.classList.remove('hidden')
+
+  results.textContent = `You got ${correctCount}/${questions.length} correct`
+}
